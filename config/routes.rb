9c611b087d0469c030d75798ee03c1069d245533
HiroFukuda <s_cluch52@hotmail.co.jp>
2015-08-02 12:35:45 +0900

    signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 VariousRecordingApp::Application.routes.draw do
-  get "users/new"
-  get "static_pages/home"
+  root 'static_pages#home'
+  get 'users/new'
+  get 'static_pages/home'
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

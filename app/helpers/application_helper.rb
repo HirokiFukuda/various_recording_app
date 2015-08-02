9b395c9e9abfd24520fad71af7583c30625680ee
HiroFukuda <s_cluch52@hotmail.co.jp>
2015-08-02 15:58:26 +0900

    ホームページに要素を追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  # ページごとの完全なタイトルを返す
+  def full_title(page_title)
+    base_title = "VRA"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 VariousRecordingApp::Application.routes.draw do
-  root 'static_pages#home'
-  get 'users/new'
-  get 'static_pages/home'
+  root      'static_pages#home'
   resources :users
+  get       '/home',         to: 'static_pages#home'
+  get       '/signup',       to: 'users#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
